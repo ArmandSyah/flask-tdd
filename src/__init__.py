27c460252195c0c0fda57cbe28618191d61ba0db
armand.syahtama <armand.syahtama@gmail.com>
2021-03-21 09:34:05 -0400

    set up docker for application

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -7,7 +9,9 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('src.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS') 
+app.config.from_object(app_settings)      
+
 
 class Ping(Resource):
     def get(self):
@@ -16,5 +20,7 @@ class Ping(Resource):
             'message': 'Pong Lenis'
         }
 
+import sys
+print(app.config, file=sys.stderr)
 
 api.add_resource(Ping, '/ping')
